ccdac5772dc929bd2e175a126ad7e3fcf1c6599f
Jay Rajput <jayrajput512199@gmail.com>
2024-06-06 02:17:43 +0530

    post created

--- foodrecipe/foodapp/views.py
+++ foodrecipe/foodapp/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 
+from foodapp.models import *
+
 
 # Create your views here.
 def foodview(request):
+    if request.method == "POST":
+        data = request.POST
+
+        image = request.FILES.get('food_image')
+        name = data.get('food_name')
+        description = data.get('food_description')
+
+        Food.objects.create(
+            food_image = image,
+            food_name = name,
+            food_description = description
+        )
+
     return render(request, 'food.html')
